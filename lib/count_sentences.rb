71e70e2c6b4db68aae3ca54b0599ea7765092366
sparkbold-git <sparkboldstudio@gmail.com>
2018-08-30 16:27:43 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    end_with?(".")
   end
 
   def question?
-
+    end_with?("?")
   end
 
   def exclamation?
-
+    end_with?("!")
   end
 
   def count_sentences
+
+    self.split(/[.!?]+/).size
 
   end
 end
